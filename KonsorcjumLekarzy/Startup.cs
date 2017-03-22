03d737600ac57f2ffd889873775b3f95bc908784
DESKTOP-F8R7JC5\Root <wczasiesuszyszosasucha@wp.pl>
2017-03-22 22:32:44 +0100

    Creating repository.

---
+++ KonsorcjumLekarzy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KonsorcjumLekarzy.Startup))]
+namespace KonsorcjumLekarzy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
