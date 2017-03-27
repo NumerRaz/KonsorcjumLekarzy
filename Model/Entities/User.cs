de3b2c06db07a021a31e817cfac892c128ada3cf
Paweł Kapała <pawelkapala69@gmail.com>
2017-03-28 00:35:59 +0200

    Add Model Entities

---
+++ Model/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entities
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public decimal PhoneNumber { get; set; }
+        [Required]
+        public string EmailAdress { get; set; }
+        [Key]
+        [ForeignKey("UserTypeID")]
+        public virtual UserType UserType { get; set; }
+    }
+}
