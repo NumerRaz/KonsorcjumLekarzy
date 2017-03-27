21f4fb0b8f9a736ccfed283a7364b6a7d9e38233
Paweł Kapała <pawelkapala69@gmail.com>
2017-03-27 23:10:14 +0200

    Setting NinjectDependencyResolver

--- KonsorcjumLekarzy/KonsorcjumLekarzy/App_Start/NinjectWebCommon.cs
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,8 @@ namespace KonsorcjumLekarzy.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(
+                new KonsorcjumLekarzy.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
