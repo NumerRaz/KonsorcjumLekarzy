02c6d43a71e82d00f354521476f68317f980e0c8
Paweł Kapała <pawelkapala69@gmail.com>
2017-03-28 22:33:05 +0200

    Test commit

--- KonsorcjumLekarzy/KonsorcjumLekarzy/Properties/AssemblyInfo.cs
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebUI")]
+[assembly: AssemblyTitle("KonsorcjumLekarzy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebUI")]
+[assembly: AssemblyProduct("KonsorcjumLekarzy")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
