cc626f699850a0674bcd9654b58fca2ac2a9ee6e
cimichal <cieslamichal1gmail.com>
2017-05-11 22:39:41 +0200

    Added DTOs models. Add new action to home controller.

--- KonsorcjumLekarzy/KonsorcjumLekarzy/Controllers/HomeController.cs
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/Controllers/HomeController.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KonsorcjumLekarzy.Database.Model;
+using KonsorcjumLekarzy.Models.DTOs;
+using KonsorcjumLekarzy.Services;
 using Microsoft.AspNet.Identity;
 
 namespace KonsorcjumLekarzy.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IGenericService<Doctor> _doctorService;
+        private readonly IGenericService<Patient> _patientService;
+        private readonly IGenericService<Specialization> _specializationService;
+        private readonly IGenericService<Visit> _visitService;
+        private readonly IGenericService<ApplicationUser> _userService;
+
+        public HomeController(IGenericService<Doctor> doctorService, IGenericService<Patient> patientService, IGenericService<Specialization> specializationService, IGenericService<Visit> visitService, IGenericService<ApplicationUser> userService)
+        {
+            _doctorService = doctorService;
+            _patientService = patientService;
+            _specializationService = specializationService;
+            _visitService = visitService;
+            _userService = userService;
+        }
 
         [Authorize]
         public ActionResult Index()
@@ -20,6 +37,41 @@ namespace KonsorcjumLekarzy.Controllers
         public ActionResult UserAuthorize()
         {
             return View();
+        }
+
+        public JsonResult GetInitialData()
+        {
+            var initDto = new InitDTO();
+            var doctorsDto = _doctorService.EntietiesList().Select(d => new DoctorDTO()
+            {
+                DoctorId = d.DoctorId,
+                FirstName = d.FirstName,
+                LastName = d.LastName,
+                BirthDay = d.BirthDay,
+                UserDto = new UserDTO()
+                {
+                    id = _userService.EntietiesList().Where(u => u.Id == d.UserId).Select(s => s.Id).FirstOrDefault(),
+                    UserName = _userService.EntietiesList().Where(u => u.Id == d.UserId).Select(s => s.UserName).FirstOrDefault(),
+                }
+            }).ToList();
+
+            var patientsDto = _patientService.EntietiesList().Select(p => new PatientDTO()
+            {
+                PatientId = p.PatientId,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                BirthDay = p.BirthDay,
+                UserDto = new UserDTO()
+                {
+                    id = _userService.EntietiesList().Where(u => u.Id == p.UserId).Select(s => s.Id).FirstOrDefault(),
+                    UserName = _userService.EntietiesList().Where(u => u.Id == p.UserId).Select(s => s.UserName).FirstOrDefault(),
+                }
+            }).ToList();
+
+            initDto.DoctorDto = doctorsDto;
+            initDto.PatientDto = patientsDto;
+
+            return Json(initDto, JsonRequestBehavior.AllowGet);
         }
 
     }
---
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/Models/DTOs/InitDTO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using KonsorcjumLekarzy.Database.Model;
+
+namespace KonsorcjumLekarzy.Models.DTOs
+{
+    public class InitDTO
+    {
+        public List<DoctorDTO> DoctorDto  { get; set; }
+        public List<VisitDTO> VisitDto { get; set; }
+        public List<SpecializationDTO> SpecializationDto { get; set; }
+        public List<UserDTO> UserDto { get; set; }
+        public List<PatientDTO> PatientDto { get; set; }
+    }
+
+    public class PatientDTO
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public UserDTO UserDto { get; set; }
+    }
+
+    public class UserDTO 
+    {
+        public string id { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    public class SpecializationDTO
+    {
+        public int SpecializationId { get; set; }
+        public string SpecializationName { get; set; }
+        public string SpecializationDescription { get; set; }
+    }
+
+    public class VisitDTO
+    {
+        public int VisitID { get; set; }
+        public DoctorDTO DoctorDto { get; set; }
+        public PatientDTO PatientDto { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+        public bool Confirmation { get; set; }
+    }
+
+    public class DoctorDTO
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public UserDTO UserDto { get; set; }
+    }
+}
