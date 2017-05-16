6d227dab679365a4c188bd48c2d504c8c0f94750
cimichal <cieslamichal1gmail.com>
2017-05-16 22:15:46 +0200

    Add new component for single vist. Add moment.js libary for manage data in js. Change styling.

---
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/Angular/pages/bookingVisit/singleVisit/singleVisit.component.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    "use strict";
+
+    var module = angular.module("BlurAdmin.pages.bookingvisit");
+
+    function controller() {
+        var vm = this;
+        
+        vm.$onInit = function () {
+            console.log(vm.visit);
+        }
+    }
+
+    module.component("singleVisit",
+    {
+        templateUrl: "Angular/pages/bookingVisit/singleVisit/singleVisit.html",
+        bindings: {
+            visit: "<"
+        },
+        controllerAs: "vm",
+        controller: controller
+    });
+})();
--- KonsorcjumLekarzy/KonsorcjumLekarzy/Angular/pages/bookingVisit/visit.component.js
+++ KonsorcjumLekarzy/KonsorcjumLekarzy/Angular/pages/bookingVisit/visit.component.js
@@ -10,8 +10,10 @@
         var parseInitData = function() {
             vm.initData.data = global_InitData;
             for (var visit = 0; visit < global_InitData.Visits.length; visit++) {
+                let elementDate = moment(global_InitData.Visits[visit].StartDate).toDate();
+
                 vm.initData.allVisits.push({
-                    startDate: global_InitData.Visits[visit].StartDate,
+                    startDate: elementDate,
                     doctorId: global_InitData.Visits[visit].DoctorId,
                     patientId: global_InitData.Visits[visit].PatientId,
                     duration: global_InitData.Visits[visit].Duration,
@@ -20,6 +22,32 @@
             }
         };
 
+        var getConfirmedVisits = function() {
+            let confirmedVisits = [];
+            confirmedVisits.filter(function(element, index, array) {
+                return element.Confirmation === true;
+            });
+            vm.initData.confirmedVisits = confirmedVisits;
+        };
+
+        var getLastVisit = function() {
+
+            let allVisits = vm.initData.data.Visits;
+
+            allVisits.sort(function(a, b) {
+                let jsdateB = moment(b.StartDate).toDate();
+                let jsdateA = moment(a.StartDate).toDate();
+                return new Date(jsdateB) - new Date(jsdateA);
+            });
+
+            vm.initData.nestVisit.push({
+                startDate: moment(allVisits[0].StartDate).toDate(),
+                doctorId: allVisits[0].DoctorId,
+                patientId: allVisits[0].PatientId,
+                duration: allVisits[0].Duration,
+                confirmation: allVisits[0].Confirmation
+            });
+        }
 
         vm.$onInit = function () {
             vm.initData = {};
@@ -27,6 +55,8 @@
             vm.initData.confirmedVisits = [];
             vm.initData.nestVisit = [];
             parseInitData();
+            getLastVisit();
+            getConfirmedVisits();
         }
     }
 
